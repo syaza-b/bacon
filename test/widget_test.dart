50ec3eabbcd9524cd6519b024f92799744e92e77
syaza-b <128579223+syaza-b@users.noreply.github.com>
2023-04-24 15:40:18 +0800

    Failed merged reverted build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bacon/login.dart';
+import 'package:bacon/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SignInPage());
+    await tester.pumpWidget(const Bacon());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
